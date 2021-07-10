b7b71eb2b2c30b1e071a526421359ee577fc8000
Nelson Barrientos <62807501+nbbarrientos@users.noreply.github.com>
2021-07-09 20:12:51 -0400

    Update sqtl_filter.sh

--- QTLtools/sqtl_filter.sh
+++ QTLtools/sqtl_filter.sh
@@ -3,7 +3,7 @@
 ### Shell script to filter QTLtools nominal results file ###
 
 #Flags:
-main_dir="/project/cphg-millerlab/CAD_QTL/coronary_QTL/transcriptome/LeafCutter/sQTL_mapping/QTLtools_runs/nominal_analyses/qtltools_finalrun_PCs_Age_Sex"
+main_dir="PATH_TO_QTLTOOLS_OUTPUT"
 
 module load gcc/7.1.0 openmpi/3.1.4 R/4.0.0
 
