fb56692f53e565f6aa4e23f01d9a83ab629b0b8d
Nelson Barrientos <62807501+nbbarrientos@users.noreply.github.com>
2021-07-09 20:33:57 -0400

    Update PCA.R

--- VCF_prep/PCA.R
+++ VCF_prep/PCA.R
@@ -1,4 +1,4 @@
-### The purpose of this script is generating PCs for ~171 VCF files using SNPRelate BioConductor package
+### The purpose of this script is generating PCs for VCF files using SNPRelate BioConductor package
 ### Code adapted from https://github.com/zhengxwen/SNPRelate
 ### If package not installed:
 
@@ -13,7 +13,7 @@ library(gdsfmt)
 library(data.table)
 
 ##VCF file name to read in for GDS filetype conversion: 
-vcf.fn <- "/project/cphg-millerlab/CAD_QTL/coronary_QTL/transcriptome/LeafCutter/PCA/UVA_coronary_hg38_merged_imputed_filtered.vcf.gz"
+vcf.fn <- "PATH_TO_VCF_FILE"
 
 ##Convert to GDS using:
 snpgdsVCF2GDS(vcf.fn, paste0("gds_QTL_", Sys.Date()), method="biallelic.only")
@@ -36,17 +36,9 @@ pca <- snpgdsPCA(genofile, snp.id=pcasnp.id, num.thread=4)
 
 ### Add in population information
 sample.id <- read.gdsn(index.gdsn(genofile, "sample.id")) 
-#print(sample.id)
 pcaout <- as.data.frame(pca$eigenvect)
-#print(pcaout) 
-#head(pcaout)
-#print(pcaout)
-#write.table(pcaout, "PCs_but_no_sampleid.txt", sep="\t", row.names=T, quote=F)
-
 new <- t(pcaout)
-#print(new)
 colnames(new) <- sample.id
-#print(new)
 
-write.table(new, "/project/cphg-millerlab/CAD_QTL/coronary_QTL/transcriptome/LeafCutter/PCA/UVA_Coronary_Artery_PC.txt", sep="\t", row.names=T, col.names=T, quote=F)
+write.table(new, "PATH_TO_OUTPUT_DIR/NAME_OF_OUPUT_FILE", sep="\t", row.names=T, col.names=T, quote=F)
 
